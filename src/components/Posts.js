bf1ff0551361cc156bc2755f1103f6d35c57629d
AleksandrKompanetc <moneymacker7@gmail.com>
2024-11-04 23:54:29 +0200

    changing component postSlice

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PostItem from './PostItem';
+import { useDispatch } from 'react-redux';
+import { getPosts } from '../features/post/postSlice';
 
 const Posts = () => {
+  const dispatch = useDispatch()
   return (
     <div>
       <button
         type='submit'
         className='bg-lime-300 hover:bg-lime-400 transition-all p-2 text-sm mb-2'  
+        onClick={() => dispatch(getPosts())}
       >
         Get posts
       </button>
